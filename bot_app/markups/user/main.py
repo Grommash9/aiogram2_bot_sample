c1df8b653d2fa66f6050b8d1248d81bcb67961ed
Grommash9 <67575598+Grommash9@users.noreply.github.com>
2022-12-25 09:05:17 +0200

    new req + new ssl guide

--- bot_app/markups/user/main.py
+++ bot_app/markups/user/main.py
@@ -4,5 +4,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 def main_menu():
     m = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     m.insert(KeyboardButton('Меню'))
-    m.insert(KeyboardButton('sad', ))
+    m.insert(KeyboardButton('sad'))
     return m
