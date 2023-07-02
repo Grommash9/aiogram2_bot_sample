fec965ce7abc5c67a5eff125319c9fc2abd9a905
oleksandr <prudnikov21@icloud.com>
2023-07-02 22:57:37 +0100

    dump change

--- dump.sql
+++ dump.sql
@@ -1,16 +1,14 @@
 DROP TABLE IF EXISTS `data_tg_user`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `data_tg_user`
 (
     `user_id`   bigint     NOT NULL,
     `name`      varchar(200) DEFAULT NULL,
     `user_name` varchar(100) DEFAULT NULL,
-    `balance`   float        DEFAULT NULL COMMENT '' balance '',
+    `balance`   float        DEFAULT NULL,
     `language`  varchar(4) NOT NULL,
     PRIMARY KEY (`user_id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
