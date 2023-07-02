2127969435f13edbbc619573283eaef62d6296bf
oleksandr <prudnikov21@icloud.com>
2023-07-02 22:50:11 +0100

    dump change

--- dump.sql
+++ dump.sql
@@ -1,11 +1,16 @@
-CREATE TABLE IF NOT EXISTS `data_tg_user` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY,
-  `user_id` INT,
-  `name` VARCHAR(255),
-  `user_name` VARCHAR(255),
-  `language` VARCHAR(255)
-);
+DROP TABLE IF EXISTS `data_tg_user`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `data_tg_user`
+(
+    `user_id`   bigint     NOT NULL,
+    `name`      varchar(200) DEFAULT NULL,
+    `user_name` varchar(100) DEFAULT NULL,
+    `balance`   float        DEFAULT NULL COMMENT '' balance '',
+    `language`  varchar(4) NOT NULL,
+    PRIMARY KEY (`user_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT IGNORE INTO `data_tg_user` (`user_id`, `name`, `user_name`, `language`)
-VALUES
-  (12345, 'John Doe', 'johndoe', 'uk');
